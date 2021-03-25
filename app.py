da413c99d833438816bcb5fe0e75b41d39b967e4
Isaac <isaac061@gmail.com>
2021-03-24 21:35:54 -0300

    add search

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from settings.conf import KEY_YOUTUBE
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from pyyoutube import Api as api_youtube
 app = Flask(__name__)
 
 
 @app.route('/lista', methods=['GET'])
 def youtube():
-    print('yoooo')
-    print(KEY_YOUTUBE)
+    search = request.args.get("search", None)
     status_code = "200"
     try:
         api = api_youtube(api_key=KEY_YOUTUBE)
-        videos = api.search_by_keywords(q="surfing", search_type=["channel", "video", "playlist"], limit=10)
+        videos = api.search_by_keywords(q=search, search_type=["channel", "video", "playlist"], limit=10)
 
         resp = {
-            "mensaje": "salida",
+            "search": search,
             "videos": videos
         }
     except Exception as e:
-
         resp = {
-            "errror": "Ups hubo un problema"
+            "error": "Ups hubo un problema"
         }
         status_code = 404
 
